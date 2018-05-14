aa6849d53565ed1cebc0f0f39f4c9288c7527d66
karolsw2 <karol.sw3@gmail.com>
2018-05-14 23:35:44 +0200

    Add basic file structure

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp')
+var pug = require('gulp-pug')
+var stylus = require('gulp-stylus')
+
+gulp.task('html', function () {
+  return gulp.src('src/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('css', function () {
+  return gulp.src('src/**/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('default', [ 'html', 'css' ])
