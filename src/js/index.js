7aff65f8e61d120563ecb6cd5ddcafaf93862c38
karolsw2 <karol.sw3@gmail.com>
2018-05-21 22:56:56 +0200

    Add on scroll revealing animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
 document.addEventListener('scroll', () => {
   var scroll = document.body.scrollTop
   document.getElementsByClassName('header__image')[0].style.top = scroll * 0.4 - 30
+
+  Array.from(document.getElementsByClassName('revealOnScroll')).forEach((element, index) => {
+    let rect = element.getBoundingClientRect()
+    if (scroll > (rect.top + window.innerHeight / 3)) {
+      if (!element.classList.contains('revealAnimation')) {
+        element.classList.add('revealAnimation')
+      }
+    }
+  })
 })
